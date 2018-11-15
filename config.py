ffac150f468498d334e358c4d06560b033dbb3ae
Jyneda <jyneda@gmail.com>
2018-11-15 12:08:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 DEBUG = False
 
-FLASK_SECRET_KEY = "" # use random string
+FLASK_SECRET_KEY = '#$%^%$#^%GFDSDFGHGFDSWERE' # use random string, for example, "#$%^%$#^%$TRFGHJHGFDSDFGHGFDSWERE" 
 
 # DB_CONFIG = '<database>[+<orm_lib>]://[<user>[:<password>]]<host>[:<port>]/[db_name][?charset=utf8]'
 # ex: DB_CONFIG = 'mysql+pymysql://user:pass@localhost:3306/db_name?charset=utf8'
@@ -10,4 +10,4 @@ DB_POOL_RECYCLE = 600
 QUERY_LIMIT = 100
 REQUEST_TIMEOUT = 10
 
-CSM_HOST = ''
+CSM_HOST = '140.113.199.13'  # IoTtalk server's 'IP' or 'DomainName', for example, '140.113.199.13'
