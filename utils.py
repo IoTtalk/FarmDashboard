81fd668f04744d6fd176d99c2d2112f5d76cf624
Ksoy <ksoyam95@gmail.com>
2019-05-20 18:10:29 +0800

    enhence utils:row2dict

--- utils.py
+++ utils.py
@@ -7,8 +7,14 @@ def row2dict(row, str_datetime=True):
         return None
 
     d = {}
-    for column in row.__table__.columns:
-        d[column.name] = getattr(row, column.name)
-        if str_datetime and isinstance(d[column.name], datetime.datetime):
-            d[column.name] = str(d[column.name])
+    if hasattr(row, '__table__'):
+        for column in row.__table__.columns:
+            d[column.name] = getattr(row, column.name)
+            if str_datetime and isinstance(d[column.name], datetime.datetime):
+                d[column.name] = str(d[column.name])
+    if hasattr(row, '_fields'):
+        for column_name in row._fields:
+            d[column_name] = getattr(row, column_name)
+            if str_datetime and isinstance(d[column_name], datetime.datetime):
+                d[column_name] = str(d[column_name])
     return d
