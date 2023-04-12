a2fe8a6ebd266ac8695ec0996b0c0cda410ae43e
jasmine8988 <43335138+jasmine8988@users.noreply.github.com>
2023-04-12 15:17:11 +0800

    Update app.py

--- da/app.py
+++ da/app.py
@@ -1,5 +1,5 @@
-#1.解決duplicate entry，提取device_addr也就是DAI裡的reg_addr, 2.Device name要是field+DataServer, 
-#3.將data格式改成(timestamp,data)，使value = data[0][1]成立
+#1. 加入kwargs  #2. dynamic odf_function成功被dai讀取
+#但在把資料加入到cwb filed資料庫時，有出現問題
 import time
 import datetime
 import logging
@@ -16,8 +16,8 @@ log = logging.getLogger("\033[1;35m[DA]: \033[0m")
 def on_register(dan):
 	print('register successfully')
 
-class SaClass: #將sa的資料變成attribute，讓thread可以帶入到module_to_sa去執行
-    def __init__(self, host, field, field_id, profile={}, odf_list=[], alert_range={}, device_addr=None, **args):
+class SaClass:
+    def __init__(self, host, field, field_id, profile={}, odf_list=[], alert_range={}, device_addr=None, **kwargs):
         self.host = host
         self.field = field
         self.field_id = field_id
@@ -25,52 +25,50 @@ class SaClass: #將sa的資料變成attribute，讓thread可以帶入到module_t
         self.odf_list = odf_list
         self.alert_range = alert_range
         self.device_addr = device_addr   #profile['d_name']
-        self.args = args
+        
+        for odf in odf_list:
+            odf = re.sub(r'-', r'_', odf)    #解決dai讀不到odf_function的問題了
+            setattr(self, odf, lambda data, odf=odf : self.run(data, odf))  #需要多增加一個參數odf，odf_function才能得知當前的odf是誰
+        
+        self.kwargs = kwargs
+        for k, v in kwargs.items():
+      	    setattr(self, k, v)
+        
         self.device_name = field.name + '_DataServer'
         self.api_url = host  #get from config CSM_HOST
         self.device_model = 'DataServer'
         self.push_interval = 10
         self.interval = {'Dummy_Sensor': 3,}
-        
-    def AvgTD_O(self,data: list):
-	    print('df_name: AvgTD_O, data:{}'.format(str(data[0])))
     
-    def AvgTemp_O(self,data: list): #df=AvgTemp_O
-	    df = 'AvgTemp-O'
-	    try:
-	        session = db.get_session()
-	        if data:
-	        	value = float(data[0])
-	        	new_model = getattr(db.models, re.sub(r"[-_]O", "", df))(timestamp=datetime.datetime.now(),field=self.field_id, value=value)
-	        	session.add(new_model)
-	        	session.commit()
-	        	
-	        	#if df in self.alert_range:
-	        	 #   alert_min = self.alert_range[df].get('min', 0)
-	        	  #  alert_max = self.alert_range[df].get('max', 0)
-	        	   # if alert_min != alert_max and (value > alert_max or value < alert_min):
-	        	    #    dan.push('Alert-I', '{} {}'.format(df, value))
-    		
-	    except KeyboardInterrupt:
-	        log.info(self.field, ': exit')
-	        	
-	    except Exception as e:
-		    log.error('[ERROR]:', str(e))
-		    if str(e).find('mac_addr not found:') != -1:
-		        log.error('Reg_addr is not found. Try to re-register...')
-		    else:
-		        log.error('Connection failed due to unknow reasons.')
-		        time.sleep(1)
-	    finally:
-	        session.close()	
+    def run(self,data: list, odf):
+        try:
+            session = db.get_session()
+            if data:
+                value = float(data[0])
+                new_model = getattr(db.models, re.sub(r"[-_]O", "", odf))(timestamp=datetime.datetime.now(),field=self.field_id, value=value)
+                session.add(new_model)
+                session.commit()
+                
+        except KeyboardInterrupt:
+            log.info(self.field, ': exit')
+        except Exception as e:
+            log.error('[ERROR]:', str(e))
+            if str(e).find('mac_addr not found:') != -1:
+                log.error('Reg_addr is not found. Try to re-register...')
+            else:
+                log.error('Connection failed due to unknow reasons.')
+                time.sleep(1)
+        finally:
+            session.close()	
 
-def main():#執行process
+
+def main():
 	db.connect()
 	processes = []
 	
 	session = db.get_session()
     
-	for field in (session.query(db.models.field).all()): #再外面取完session,field,profile,odf_list後再帶入SaClass
+	for field in (session.query(db.models.field).all()):
 	    profile = {'d_name': field.name + '_DataServer',
 	               'dm_name': 'DataServer',
 	               'odf_list': [],
@@ -85,8 +83,7 @@ def main():#執行process
 	        profile['odf_list'].append(df.df_name)
 	        alert_range[df.df_name] = {'min': df.alert_min,
 	                                   'max': df.alert_max}
-	    
-	 
+
 	    sa = SaClass(host, field, field.id, profile, profile['odf_list'], alert_range, profile['d_name'])
 	    
 	    process = module_to_sa(sa)
