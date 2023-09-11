7fa9700079213dd5192718d050f9484345174996
jyneda <jyneda@gmail.com>
2023-09-12 00:35:18 +0800

    Enable dashboard with MQTT protocol.

--- da/DAI.py
+++ da/DAI.py
@@ -1,41 +1,108 @@
-import time
-
+import time, json
 from threading import Thread
+from copy import deepcopy
+from datetime import datetime
+
+import paho.mqtt.client as mqtt
 
 from db import db
+from da.DAN import DAN, log
 
 from config import CSM_HOST as host
-from da.DAN import DAN, log
+from config import MQTT_broker as broker
+from config import MQTT_port as mqt_port
+from config import MQTT_User as mqt_usr
+from config import MQTT_PW as mqt_pw
+from config import MQTT_encryption as mqt_encrypt
+
 
 def _run(profile, reg_addr, field, field_id, alert_range={}):
     dan = DAN()
     dan.device_registration_with_retry(profile, host, reg_addr)
+    
+    ODF_list = deepcopy(dan.selected_DF)
+    ODF_list.remove('Alert-I')
+
+    session = db.get_session()
+    def insert_into_db(odf, value, timestamp):
+        new_value = getattr(db.models, odf.replace('-O', ''))(timestamp=timestamp, field=field_id, value=value)
+        session.add(new_value)
+        session.commit()
+
+    def on_connect(client, userdata, flags, rc):
+        if not rc:
+            print('MQTT broker: {}'.format(broker))
+            if ODF_list == []:
+                print('ODF_list is not exist.')
+                return
+            topic_list=[]
+            for odf in ODF_list:
+                topic = '{}//{}'.format(reg_addr, odf)
+                topic_list.append((topic,0))
+            if topic_list != []:
+                r = client.subscribe(topic_list)
+                if r[0]: print('Failed to subscribe topics. Error code:{}'.format(r))
+        else: print('Connect to MQTT borker failed. Error code:{}'.format(rc))
+
+    def on_disconnect(client, userdata,  rc):
+        print('MQTT Disconnected. Re-connect...')
+        client.reconnect()
+
+    def on_message(client, userdata, msg):
+        samples = json.loads(msg.payload)
+        device_id, ODF_name = msg.topic.split('//')
+        ODF_timestamp = samples['samples'][0][0]
+        ODF_data = samples['samples'][0][1][0]
+        print('{}: {}, {}, {}, {}'.format(ODF_timestamp, field, device_id, ODF_name, ODF_data))
+        insert_into_db(ODF_name, ODF_data, ODF_timestamp)
+        log.debug(field, ODF_name, ODF_data)
+        check_alert(client, device_id, ODF_name, ODF_data)        
+
+    def MQTT_config(client, broker, port, user, pw, encryption=False):
+        client.username_pw_set(user, pw)
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.on_disconnect = on_disconnect
+        if encryption: client.tls_set()
+        client.connect(broker, port, keepalive=60)
+
+    def mqtt_pub(client, deviceId, IDF, data):
+        topic = '{}//{}'.format(deviceId, IDF)
+        sample = [str(datetime.today()), data]
+        payload  = json.dumps({'samples':[sample]})
+        status = client.publish(topic, payload)
+        if status[0]: print('topic:{}, status:{}'.format(topic, status))
+
+    def check_alert(client, device_id, odf, odf_data):
+        if odf not in alert_range: return
+        alert_min = alert_range[odf].get('min', 0)
+        alert_max = alert_range[odf].get('max', 0)
+        if alert_min != alert_max and (odf_data > alert_max or odf_data < alert_min):
+            if client: mqtt_pub(client, device_id, odf, '{} {}'.format(odf, odf_data))
+            else: dan.push('Alert-I', '{} {}'.format(odf, odf_data))
+            print('Alert-I: {}, {}, {}'.format(device_id, odf, odf_data))
+
+    if broker:
+        mqttc = mqtt.Client()
+        MQTT_config(mqttc, broker, mqt_port, mqt_usr, mqt_pw, mqt_encrypt)
+        mqttc.loop_forever()    
+
     while True:
         try:
             # Pull data
-            session = db.get_session()
             for df in dan.selected_DF:
                 data = dan.pull_with_timestamp(df)
                 if data:
+                    print('{}: {}, {}'.format(field, df, data))
                     log.debug(field, df, data)
                     timestamp = data[0]
-
                     try:
                         value = float(data[1][0])
                     except Exception as e:
                         log.warning(e, ', ignore this data.')
                         continue
-
-                    new_model = getattr(db.models, df.replace('-O', ''))(timestamp=timestamp, field=field_id, value=value)
-                    session.add(new_model)
-                    session.commit()
-
-                    # alert
-                    if df in alert_range:
-                        alert_min = alert_range[df].get('min', 0)
-                        alert_max = alert_range[df].get('max', 0)
-                        if alert_min != alert_max and (value > alert_max or value < alert_min):
-                            dan.push('Alert-I', '{} {}'.format(df, value))
+                    insert_into_db(df, value, timestamp)
+                    check_alert(None, reg_addr, df, value)
             time.sleep(20)
         except KeyboardInterrupt:
             log.info(field, ': exit')
@@ -62,6 +129,7 @@ def main():
                    'dm_name': 'DataServer',
                    'df_list': ['Alert-I'],
                    'is_sim': False}
+        if broker: profile['mqtt_enable'] = True
         alert_range = {}
         query_df = (session.query(db.models.field_sensor)
                            .select_from(db.models.field_sensor)
